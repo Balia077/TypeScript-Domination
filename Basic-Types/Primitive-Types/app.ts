c2a46bd58a0c8b27701920857d0c34c766a2bcbc
Balia077 <balaramd894@gmail.com>
2026-04-06 09:21:26 +0530

    first commit

---
+++ Basic-Types/Primitive-Types/app.ts
@@ -0,0 +1,7 @@
+// Primitive Types
+
+// number, string, boolean
+
+let a = 12;     // type : number
+let b = "Balia" // type : string
+let c = true;   // type : boolean
---
+++ Basic-Types/Reference-Types/Arrays/app.ts
@@ -0,0 +1,5 @@
+// Arrays
+
+let arr = [1, 2, 3];                // type : number[]
+let arr2 = [1, 2, "a"];             // type : (string | number)[]
+let arr3 = [1, 2, {name: "abc"}]    // type : (number | {name: string})[]
