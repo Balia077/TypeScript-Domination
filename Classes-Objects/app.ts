0267a85692d9c97d92de0b5880921ea6cd10e4fb
Balia077 <balaramd894@gmail.com>
2026-04-10 10:55:40 +0530

    Comment out Device class and instances in app.ts and app.js

--- Classes-Objects/app.ts
+++ Classes-Objects/app.ts
@@ -1,10 +1,10 @@
 // Classes & Objects
 
-class Device {
-    name = "lg";
-    price = 12000;
-    category = "digital";
-}
+// class Device {
+//     name = "lg";
+//     price = 12000;
+//     category = "digital";
+// }
 
-let d1 = new Device();
-let d2 = new Device();
+// let d1 = new Device();
+// let d2 = new Device();
