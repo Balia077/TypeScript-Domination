6fd462d53351981d81c62f3eebf1182322cb5261
Balia077 <balaramd894@gmail.com>
2026-04-11 09:15:54 +0530

    Add initial implementation of Abcd class and HTML structure for Constructors example

---
+++ Classes-Objects/Constructors/app.ts
@@ -0,0 +1,29 @@
+// Constructors
+
+// class BottleMaker{
+//     constructor(public brand: string, public price: number, public color: string, public material: string){
+
+//     }
+// }
+
+// let b1 = new BottleMaker("Milton", 100, "Red", "metal");
+// let b2 = new BottleMaker("Cello", 100, "White", "Plastic");
+
+// class Music{
+//     constructor(public name: string, public artist: string, public length: number, public free: boolean, public thumbnail: string = "somethumbnail.jpg"){
+        
+//     }
+// }
+
+// let m1 = new Music("chal chhaiya chhaiya", "Balaram", 1200, false);
+// m1.name = "koi aur ganaa";
+
+// this keyword
+
+class Abcd{
+    name = "Balaram";
+
+    changeName(){
+        this.name = "Balia";
+    }
+}
